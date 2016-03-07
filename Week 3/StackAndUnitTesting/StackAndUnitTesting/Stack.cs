c82d9eda8f45a20b6fb7804e7cf12e22be337db9
gregfield <gregfield1996@gmail.com>
2016-03-07 15:16:06 +1300

    Stack Created
    created the stack and all associated methods

---
+++ Week 3/StackAndUnitTesting/StackAndUnitTesting/Stack.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndUnitTesting
+{
+    class Stack
+    {
+        //Linked list pointers
+        StringNode head;
+        StringNode tail;
+
+        public Stack()
+        {
+            head = null;
+            tail = null;
+        }
+
+        //Adds a string to the stack
+        public void Push(String stringToAdd)
+        {
+            //new string node to add the string node info
+            StringNode stringNodeToAdd = new StringNode(stringToAdd);
+
+            //find where to add new node to
+            if (isEmpty())
+            {
+                head = stringNodeToAdd;
+                tail = stringNodeToAdd;
+            }
+            else
+            {
+                tail.Next = stringNodeToAdd;
+                tail = stringNodeToAdd;
+            }
+        }
+
+        //returns the most recently added string and removes it from the stack
+        public String Pop()
+        {
+            String popString = Peek();
+
+            //if it only stringnode
+            if (head == tail)
+            {
+                head = null;
+                tail = null;
+            }
+            else
+            {
+                StringNode nodeWalker = head;
+
+                while (nodeWalker.Next != tail)
+                    nodeWalker = nodeWalker.Next;
+
+                //if next stringnode is tail
+                if (nodeWalker.Next == tail)
+                {
+                    tail = nodeWalker;
+                    nodeWalker.Next = null;
+                }
+            }
+
+            return popString;
+        }
+
+        //returns the most recently added string but does not remove it from the stack
+        public String Peek()
+        {
+            StringNode peekNode = tail;
+
+            if(isEmpty() == true)
+            {
+                return "Nothing to peek at. The Stack is Empty";
+            }
+            else
+            {
+                return peekNode.Info;
+            }
+        }
+
+        //returns the number of strings currently held in the stack
+        public int Count()
+        {
+            int count = 0;
+            StringNode nodeWalker = head;
+
+            while (nodeWalker != null)
+            {
+                count++;
+                nodeWalker = nodeWalker.Next;
+            }
+            return count;
+        }
+
+        //returns true if the stack contains zero elements and false otherwise
+        public bool isEmpty()
+        {
+            if(Count()==0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Week 3/StackAndUnitTesting/StackAndUnitTesting/StringNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndUnitTesting
+{
+    class StringNode
+    {
+        public StringNode Next { get; set; }
+        public String Info;
+
+        public StringNode(String info)
+        {
+            Info = info;
+            Next = null;
+        }
+    }
+}
