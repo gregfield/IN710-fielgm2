276d9dfc12c9e3948e5d60ed71df7c1b0043a230
gregfield <gregfield1996@gmail.com>
2016-03-28 18:19:26 +1300

    Started Adding unit test

---
+++ Week 6/WeatherMonitoring/WeatherMonitoringTesting/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WeatherMonitoring;
+
+namespace WeatherMonitoringTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
