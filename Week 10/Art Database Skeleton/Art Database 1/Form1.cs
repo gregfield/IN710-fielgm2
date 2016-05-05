82d673e503960f7ec555bcb1eac5fbb6591f2f49
gregfield <gregfield1996@gmail.com>
2016-05-05 20:17:38 +1200

    Working

---
+++ Week 10/Art Database Skeleton/Art Database 1/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Art_Database_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public List<Artist> artists;
+        public List<Painting> paintings;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            artists = new List<Artist>();
+            artists.Add(new Artist { Country = "France", FirstName = "Camille", LastName = "Pissarro", YearOfBirth = 1830, YearOfDeath = 1903 });
+            artists.Add(new Artist { Country = "France", FirstName = "Claude", LastName = "Monet", YearOfBirth = 1840, YearOfDeath = 1926 });
+            artists.Add(new Artist { Country = "England", FirstName = "John", LastName = "Constable", YearOfBirth = 1776, YearOfDeath = 1837 });
+            artists.Add(new Artist { Country = "Netherlands", FirstName = "Jan", LastName = "Vermeer", YearOfBirth = 1632, YearOfDeath = 1675 });
+            artists.Add(new Artist { Country = "Italy", FirstName = "Sanzio", LastName = "Raphael", YearOfBirth = 1483, YearOfDeath = 1520 });
+            artists.Add(new Artist { Country = "Spain", FirstName = "Pablo", LastName = "Picasso", YearOfBirth = 1881, YearOfDeath = 1973 });
+            artists.Add(new Artist { Country = "Norway", FirstName = "Edvard", LastName = "Munch", YearOfBirth = 1863, YearOfDeath = 1944 });
+            artists.Add(new Artist { Country = "Italy", FirstName = "Leonardo", LastName = "da Vinci", YearOfBirth = 1452, YearOfDeath = 1519 });
+            artists.Add(new Artist { Country = "Italy", FirstName = "Sandro", LastName = "Botticelli", YearOfBirth = 1445, YearOfDeath = 1510 });
+            artists.Add(new Artist { Country = "France", FirstName = "Henri", LastName = "Matisse", YearOfBirth = 1869, YearOfDeath = 1954 });
+            artists.Add(new Artist { Country = "Netherlands", FirstName = "Piet", LastName = "Mondrian", YearOfBirth = 1872, YearOfDeath = 1944 });
+            artists.Add(new Artist { Country = "United States", FirstName = "Jackson", LastName = "Pollock", YearOfBirth = 1912, YearOfDeath = 1956 });
+            artists.Add(new Artist { Country = "Netherlands", FirstName = "Vincent", LastName = "van Gogh", YearOfBirth = 1853, YearOfDeath = 1890 });
+
+            paintings = new List<Painting>();
+            paintings.Add(new Painting { Artist = "van Gogh", Title = "The Starry Night", Method = "Oil on canvas", Year = 1889, Width = 72, Height = 92 });
+            paintings.Add(new Painting { Artist = "van Gogh", Title = "Village Street in Auvers ", Method = "Oil on canvas", Year = 1890, Width = 73, Height = 92 });
+            paintings.Add(new Painting { Artist = "Pissarro", Title = "Gelee Blanche", Method = "Oil on canvas", Year = 1873, Width = 65, Height = 93 });
+            paintings.Add(new Painting { Artist = "Pissarro", Title = "Village Path", Method = "Oil on canvas", Year = 1875, Width = 72, Height = 92 });
+            paintings.Add(new Painting { Artist = "Monet", Title = "Fishing Boats Leaving the Harbor, Le Havre ", Method = "Oil on canvas", Year = 1874, Width = 60, Height = 101 });
+            paintings.Add(new Painting { Artist = "Monet", Title = "Water Lilies ", Method = "Oil on canvas", Year = 1906, Width = 88, Height = 93 });
+            paintings.Add(new Painting { Artist = "Constable", Title = "The Leaping Horse", Method = "Oil on canvas", Year = 1825, Width = 142, Height = 187 });
+            paintings.Add(new Painting { Artist = "Vermeer", Title = "Girl with a Pearl Earring", Method = "Oil on canvas", Year = 1665, Width = 45, Height = 40 });
+            paintings.Add(new Painting { Artist = "Raphael", Title = "Madonna dell Granduca ", Method = "Oil on wood", Year = 1505, Width = 84, Height = 55 });
+            paintings.Add(new Painting { Artist = "Raphael", Title = "St. George Fighting the Dragon ", Method = "Oil on wood", Year = 1825, Width = 28, Height = 21 });
+            paintings.Add(new Painting { Artist = "Munch", Title = "The Scream", Method = "Tempera on paper", Year = 1893, Width = 91, Height = 74 });
+            paintings.Add(new Painting { Artist = "da Vinci", Title = "The Last Supper", Method = "Tempera on plaster", Year = 1498, Width = 460, Height = 880 });
+            paintings.Add(new Painting { Artist = "Botticelli", Title = "The Birth of Venus", Method = "Tempera on canvas", Year = 1485, Width = 173, Height = 280 });
+            paintings.Add(new Painting { Artist = "Matisse", Title = "La Musique", Method = "Oil on canvas", Year = 1939, Width = 115, Height = 115 });
+            paintings.Add(new Painting { Artist = "Mondrian", Title = "Composition with Red, Yellow and Blue", Method = "Oil on canvas", Year = 1821, Width = 40, Height = 35 });
+            paintings.Add(new Painting { Artist = "Pollock", Title = "The Key", Method = "Oil on canvas", Year = 1946, Width = 84, Height = 213 });
+            paintings.Add(new Painting { Artist = "Picasso", Title = "The Three Musicians", Method = "Oil on canvas", Year = 1921, Width = 200, Height = 222 });
+
+
+
+        }
+
+        //------------------------------------------------------
+        // btnAllPaintings
+        //------------------------------------------------------
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            paintings.ForEach(p => listBox1.Items.Add(p.ToString()));
+        }
+
+
+        //------------------------------------------------------
+        // btnArtistsFromItaly
+        //------------------------------------------------------
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            IEnumerable<Artist> italyArtists = artists.Where(a => a.Country == "Italy");
+
+            foreach (Artist a in italyArtists)
+            {
+                listBox1.Items.Add(a.ToString());
+            }
+        }
+
+        //------------------------------------------------------
+        // btnBefore1800
+        //------------------------------------------------------
+        private void btnBefore1800_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            IEnumerable<Painting> before1800 = paintings.Where(p => p.Year < 1800).OrderBy(p => p.Year);
+
+            foreach (Painting p in before1800)
+            {
+                listBox1.Items.Add(p.ToString());
+            }
+        }
+
+        //------------------------------------------------------
+        // btnOldest
+        //------------------------------------------------------
+        private void btnOldest_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            IEnumerable<Painting> oldest = from p in paintings where p.Year == paintings.Min(y => y.Year) select p;
+
+            foreach (Painting p in oldest)
+            {
+                listBox1.Items.Add(p.ToString());
+            }
+        }
+
+        //------------------------------------------------------
+        // byThisArtist
+        //------------------------------------------------------
+        private void button6_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            string inputArtist = textBox1.Text.ToString();
+
+            IEnumerable<Painting> artistPaintings = paintings.Where(p => p.Artist == inputArtist);
+            foreach (Painting p in artistPaintings)
+            {
+                listBox1.Items.Add(p.ToString());
+            }
+        }
+
+        //------------------------------------------------------
+        // btnNbyCountry
+        //------------------------------------------------------
+        private void btnNbyCountry_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            var numPaintingsPerCountry = from p in paintings
+                                         join a in artists
+                                         on p.Artist equals a.LastName
+                                         group p by a.Country;
+            foreach (var num in numPaintingsPerCountry)
+            {
+                listBox1.Items.Add(num.Count() + " paintings from " + num.Key);
+            }
+        }
+
+        //------------------------------------------------------
+        // btnArtistGroupedByCountry
+        //------------------------------------------------------
+        private void button8_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            var artistByCountry = artists.GroupBy(a => a.Country);
+
+            foreach (var a in artistByCountry)
+            {
+                listBox1.Items.Add(a.Key + ":");
+                foreach (var b in a)
+                {
+                    listBox1.Items.Add("\t\t" + b.FirstName + " " + b.LastName);
+                }
+            }
+        }
+
+        //------------------------------------------------------
+        // btnDutchPainters
+        //------------------------------------------------------
+        private void button7_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            IEnumerable<Painting> dutchPaintings = from p in paintings
+                                                   join a in artists
+                                                   on p.Artist equals a.LastName
+                                                   where a.Country == "Netherlands"
+                                                   select p;
+
+            foreach (Painting p in dutchPaintings)
+            {
+                listBox1.Items.Add(p.ToString());
+            }
+        }
+
+        //------------------------------------------------------
+        // btnJoinTables
+        //------------------------------------------------------
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            var paintingsInfo = from p in paintings
+                          join a in artists
+                          on p.Artist equals a.LastName
+                          select new { p.Title, a.FirstName, a.LastName, a.Country };
+
+            foreach (var p in paintingsInfo)
+            {
+                listBox1.Items.Add(p.FirstName + " " + p.LastName + "\t\t " + p.Country + " \t\t" + p.Title);
+            }
+        }
+
+        //------------------------------------------------------
+        // btnFrenchOrItalian
+        //------------------------------------------------------
+        private void button9_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            var frenchItalian = from p in paintings
+                                  join a in artists
+                                  on p.Artist equals a.LastName
+                                  where a.Country == "Italy" || a.Country == "France"
+                                  orderby p.Artist
+                                  select new { a.Country, p.Artist, p.Title };
+
+            foreach (var f in frenchItalian)
+            {
+                listBox1.Items.Add(f.Artist + " \t\t" + f.Country + " \t\t" + f.Title);
+            }
+        }
+
+ 
+    }
+}
---
+++ Week 9/XML/XML/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace XML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Band = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Genre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Venue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.showAllGigsbtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Band,
+            this.Genre,
+            this.Venue,
+            this.Date,
+            this.Time});
+            this.dataGridView1.Location = new System.Drawing.Point(26, 29);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(543, 254);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Band
+            // 
+            this.Band.HeaderText = "Band";
+            this.Band.Name = "Band";
+            // 
+            // Genre
+            // 
+            this.Genre.HeaderText = "Genre";
+            this.Genre.Name = "Genre";
+            // 
+            // Venue
+            // 
+            this.Venue.HeaderText = "Venue";
+            this.Venue.Name = "Venue";
+            // 
+            // Date
+            // 
+            this.Date.HeaderText = "Date";
+            this.Date.Name = "Date";
+            // 
+            // Time
+            // 
+            this.Time.HeaderText = "Time";
+            this.Time.Name = "Time";
+            // 
+            // showAllGigsbtn
+            // 
+            this.showAllGigsbtn.Location = new System.Drawing.Point(26, 315);
+            this.showAllGigsbtn.Name = "showAllGigsbtn";
+            this.showAllGigsbtn.Size = new System.Drawing.Size(122, 51);
+            this.showAllGigsbtn.TabIndex = 1;
+            this.showAllGigsbtn.Text = "Show All Gigs";
+            this.showAllGigsbtn.UseVisualStyleBackColor = true;
+            this.showAllGigsbtn.Click += new System.EventHandler(this.showAllGigsbtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 459);
+            this.Controls.Add(this.showAllGigsbtn);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Band;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Genre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Venue;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Date;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Time;
+        private System.Windows.Forms.Button showAllGigsbtn;
+    }
+}
+
