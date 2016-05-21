92a37588f7b4c365414d8f85436ed75aec7b11fd
gregfield <gregfield1996@gmail.com>
2016-05-21 17:08:50 +1200

    Started Dog Selector

---
+++ Week 12/DogSelector/DogSelector/Controllers/DogSelectController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DogSelector.Controllers
+{
+    public class DogSelectController : Controller
+    {
+        // GET: DogSelect
+        public ActionResult ShowSelect()
+        {
+            return View();
+        }
+    }
+}
---
+++ Week 12/DogSelector/DogSelector/Models/Dog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DogSelector.Models
+{
+    public enum EScale { Low, Medium, High, NoPreference}
+    public enum ELength { Short, Medium, Long}
+    public enum ESize { Small, Medium, Large}
+
+    public class Dog
+    {
+        public string BreedName { get; set; }
+        public string DisplayName { get; set; }
+        public string ImageName { get; set; }
+        public EScale ActivityLevel { get; set; }
+        public EScale SheddingLevel { get; set; }
+        public EScale GroomingLevel { get; set; }
+        public EScale IntelligenceLevel { get; set; }
+        public bool GoodWithChildren { get; set; }
+        public bool Drools { get; set; }
+        public ELength CoatLength { get; set; }
+        public ESize Size { get; set; }
+    }
+}
